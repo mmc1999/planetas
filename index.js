765f9d04895a9a491b0eed4823a29882cee2e142
matias centorbi <mcentorbi161999@gmail.com>
2021-08-24 22:35:56 -0300

    Listoo solo faltan algunos estilos y ya esta

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const $iconoBurguer = document.querySelector(".iconoBurguer"),
 
 
 
+
 document.addEventListener("click", e => {
     if (e.target.matches(".iconoBurguer")) return $navPlanetas.classList.toggle("active1")
 
@@ -65,40 +66,37 @@ const traerDatos = async () => {
 
 document.addEventListener("DOMContentLoaded", traerDatos);
 
+
+
+
 function mostrarDatos(datos) {
+    
     datos.forEach((data) => {
         let url = location.pathname,
             indice = url.indexOf("."),
             nombre = url.substring(1, indice);
         console.log(nombre)
-        let $clone = document.importNode($templateDos, true);
-        if (nombre === data.name.toLowerCase()) {
-            borde(data.name.toLowerCase())
-            document.querySelector(".imagenPlaneta").src = data.images.planet;
-            document.querySelector(".imagenPlaneta").alt = data.name;
-            document.querySelector(".imagenPlanetaStructure").src = data.images.internal;
-            document.querySelector(".imagenPlanetaStructure").alt = `Imagen de la estructura interna de ${data.name}`;
-            document.querySelector(".imagenPlaneta2").src = data.images.planet;
-            document.querySelector(".imagenPlanetaInternal").src = data.images.geology;
-            document.querySelector(".imagenPlanetaInternal").alt = `Imagen de la geologia de ${data.name}`;
-            document.querySelector(".nombrePlaneta").textContent = data.name;
-            document.querySelector(".infoOverview").textContent = data.overview.content;
-            document.querySelector(".infoStructure").textContent = data.structure.content;
-            document.querySelector(".infoSurface").textContent = data.geology.content;
-            document.querySelector(".wikipediaOverview").href = data.overview.source;
-            document.querySelector(".wikipediaStructure").href = data.structure.source;
-            document.querySelector(".wikipediaSurface").href = data.geology.source;
-            $clone.querySelector(".TiempoRotacion").textContent = data.rotation;
-            $clone.querySelector(".tiempoRevolucion").textContent = data.revolution;
-            $clone.querySelector(".radius").textContent = data.radius;
-            $clone.querySelector(".grados").textContent = data.temperature;
-            $fragment.appendChild($clone);
-
-        }
-    }) //TERMINA EL ARRAY
+        if(nombre === data.name.toLowerCase()) {
+            pintarDataJson(data);
+            //ESTA ES LA MEJOR MANERA PERO ME FALTA MERCURIO QUE NO SE COMO TRAERLO SIN QUE DESPUES ME LO PINTE EN LAS OTRAS HOJAS HTML
+        } else if("mercury" === data.name.toLowerCase()) {
+            pintarDataJson(data);
+        } 
+            //ESTA ES LA MEJOR MANERA PERO ME FALTA MERCURIO QUE NO SE COMO TRAERLO SIN QUE DESPUES ME LO PINTE EN LAS OTRAS HOJAS HTML
+        
+        /*for (let i = 0; i < datos.length; i++) {
+            if("Mercury" === datos[i].name) {
+                pintarDataJson(datos, i);
+                break
+            } 
+            if("Venus" === datos[i].name) {
+                pintarDataJson(datos, i)
+            } 
+        }*/
+     //TERMINA EL ARRAY
     $acaTemplateDos.appendChild($fragment)
+    })
 }
-
 
 const borde = (nombre1) => {
     ponerClase(0, "419EBB", nombre1)
@@ -110,30 +108,52 @@ const borde = (nombre1) => {
     ponerClase(6, "1EC1A2", nombre1)
     ponerClase(7, "2D68F0", nombre1)
 }
-/*
+
 const pintarDataJson = (data) => {
     let $clone = document.importNode($templateDos, true);
-            borde(data.name.toLowerCase())
-            document.querySelector(".imagenPlaneta").src = data.images.planet;
-            document.querySelector(".imagenPlaneta").alt = data.name;
-            document.querySelector(".imagenPlanetaStructure").src = data.images.internal;
-            document.querySelector(".imagenPlanetaStructure").alt = `Imagen de la estructura interna de ${data.name}`;
-            document.querySelector(".imagenPlaneta2").src = data.images.planet;
-            document.querySelector(".imagenPlanetaInternal").src = data.images.geology;
-            document.querySelector(".imagenPlanetaInternal").alt = `Imagen de la geologia de ${data.name}`;
-            document.querySelector(".nombrePlaneta").textContent = data.name;
-            document.querySelector(".infoOverview").textContent = data.overview.content;
-            document.querySelector(".infoStructure").textContent = data.structure.content;
-            document.querySelector(".infoSurface").textContent = data.geology.content;
-            document.querySelector(".wikipediaOverview").href = data.overview.source;
-            document.querySelector(".wikipediaStructure").href = data.structure.source;
-            document.querySelector(".wikipediaSurface").href = data.geology.source;
-            $clone.querySelector(".TiempoRotacion").textContent = data.rotation;
-            $clone.querySelector(".tiempoRevolucion").textContent = data.revolution;
-            $clone.querySelector(".radius").textContent = data.radius;
-            $clone.querySelector(".grados").textContent = data.temperature;
-            $fragment.appendChild($clone);
-}*/
+    borde(data.name.toLowerCase())
+    if("mercury" !== data.name.toLowerCase()) document.querySelector(".articleInfoEspecifica").style.display = "none"
+    document.querySelector(".imagenPlaneta").src = data.images.planet;
+    document.querySelector(".imagenPlaneta").alt = data.name;
+    document.querySelector(".imagenPlanetaStructure").src = data.images.internal;
+    document.querySelector(".imagenPlanetaStructure").alt = `Imagen de la estructura interna de ${data.name}`;
+    document.querySelector(".imagenPlaneta2").src = data.images.planet;
+    document.querySelector(".imagenPlanetaInternal").src = data.images.geology;
+    document.querySelector(".imagenPlanetaInternal").alt = `Imagen de la geologia de ${data.name}`;
+    document.querySelector(".nombrePlaneta").textContent = data.name;
+    document.querySelector(".infoOverview").textContent = data.overview.content;
+    document.querySelector(".infoStructure").textContent = data.structure.content;
+    document.querySelector(".infoSurface").textContent = data.geology.content;
+    document.querySelector(".wikipediaOverview").href = data.overview.source;
+    document.querySelector(".wikipediaStructure").href = data.structure.source;
+    document.querySelector(".wikipediaSurface").href = data.geology.source;
+    $clone.querySelector(".TiempoRotacion").textContent = data.rotation;
+    $clone.querySelector(".tiempoRevolucion").textContent = data.revolution;
+    $clone.querySelector(".radius").textContent = data.radius;
+    $clone.querySelector(".grados").textContent = data.temperature;
+    $fragment.appendChild($clone);
+    /*let $clone = document.importNode($templateDos, true);
+    borde(data[i].name)
+    document.querySelector(".imagenPlaneta").src = data[i].images.planet;
+    document.querySelector(".imagenPlaneta").alt = data[i].name;
+    document.querySelector(".imagenPlanetaStructure").src = data[i].images.internal;
+    document.querySelector(".imagenPlanetaStructure").alt = `Imagen de la estructura interna de ${data[i].name}`;
+    document.querySelector(".imagenPlaneta2").src = data[i].images.planet;
+    document.querySelector(".imagenPlanetaInternal").src = data[i].images.geology;
+    document.querySelector(".imagenPlanetaInternal").alt = `Imagen de la geologia de ${data[i].name}`;
+    document.querySelector(".nombrePlaneta").textContent = data[i].name;
+    document.querySelector(".infoOverview").textContent = data[i].overview.content;
+    document.querySelector(".infoStructure").textContent = data[i].structure.content;
+    document.querySelector(".infoSurface").textContent = data[i].geology.content;
+    document.querySelector(".wikipediaOverview").href = data[i].overview.source;
+    document.querySelector(".wikipediaStructure").href = data[i].structure.source;
+    document.querySelector(".wikipediaSurface").href = data[i].geology.source;
+    $clone.querySelector(".TiempoRotacion").textContent = data[i].rotation;
+    $clone.querySelector(".tiempoRevolucion").textContent = data[i].revolution;
+    $clone.querySelector(".radius").textContent = data[i].radius;
+    $clone.querySelector(".grados").textContent = data[i].temperature;
+    $fragment.appendChild($clone);*/
+}
 
 const ponerClase = (indice, clase, nombre) => {
     if($enlacePlaneta[indice].outerText.toLowerCase() === nombre) {
